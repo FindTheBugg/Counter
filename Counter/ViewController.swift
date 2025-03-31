7814983f20bbcd844810fe25a8dda900e28bf2ec
FindTheBugg <vadim2012-2013@mail.ru>
2025-03-31 21:44:15 +0300

    Базовая задача выполнена

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet  var buttonTapped: UIButton!
+    @IBOutlet  var viewShowTapped: UILabel!
+    
+    private var valueTapped = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewShowTapped.text = String(valueTapped)
+        buttonTapped.setTitle("Посчитать нажатие", for: .normal)
+        
+        viewShowTapped.tintColor = .gray
+        buttonTapped.tintColor = .gray
+        
     }
 
-
+    @IBAction func ButtonTapped(_ sender: Any) {
+        valueTapped += 1
+        viewShowTapped.text = "Значение счетчика: \(String(valueTapped))"
+    }
+    
 }
 
